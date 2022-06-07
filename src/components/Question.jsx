4a45863d614f52325d206179b4457ea78f1d093d
Laurence Zanotti <lau_zanotti@hotmail.com>
2022-06-07 10:47:34 -0300

    Moved state and functions from child component to parent component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,71 +3,56 @@ import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 import { useOutletContext, useParams } from "react-router-dom"
 
-function Question(props) {
-    const [questions, setQuestions] = useOutletContext()
-    let params = useParams()
-    const [question, setQuestion] = useState({
-        id: null,
-        question: '',
-        alternatives: [],
-        correct_answer: ''
-    })
-    const [form, setForm] = useState({value: 0})
+function Question() {
+    const [state, setState] = useOutletContext()
+    // let params = useParams()
 
     // Controle do formulário
     function handleFormChange(event) {
         // Obter id da alternativa
         const alternative_id = parseInt(event.target.id.replace('default-', ''))
-        setForm(alternative_id)
+        setState(prevState => {
+            return {
+                ...prevState,
+                form: {value: alternative_id}
+            }})
     }
 
     // Controle do envio do formulário
     function handleSubmit(event) {
         event.preventDefault()
-        const selected_question = question.alternatives[form]
-        if (selected_question == question.correct_answer) {
+        const selected_question = state.question.alternatives[state.form.value]
+        if (selected_question == state.question.correct_answer) {
             console.log( selected_question, 'is correct');
         } else {
             console.log(selected_question, 'is incorrect');
         }
     }
-
-    // Ao obter questões, renderizar uma questão por vez
-    useEffect(() => {
-        // Selecionar a questão o array pelo índice usando parâmetro da URL
-        questions.forEach((element, index) => {
-            if (index == params.questionIndex) {
-                const selected_question = {
-                    id: index,
-                    question: element.question,
-                    alternatives: element.incorrect_answers.concat(element.correct_answer),
-                    correct_answer: element.correct_answer
-                }
-                setQuestion(selected_question)
-
-                // console.log(selected_question);
-            }
-        });
-    }, [questions])
-
+    
     return (
         <div id="question">
             <Form >
-            <fieldset>
-                <legend>{question.question}</legend>
-                {question.alternatives.map((element, index) => (
+            {<fieldset>
+                <legend>{state.question.question}</legend>
+                {state.question.alternatives.map((element, index) => (
                     <div key={`default-${index}`} className="mb-3">
                         <Form.Check 
                             onChange={e => handleFormChange(e)}
                             type={'radio'}
                             id={`default-${index}`}
                             label={`${element}`}
-                            name={`question-${question.id}`}
+                            name={`question-${state.question.id}`}
                         />
                     </div>
                 ))}
-            </fieldset>
-            <Button variant="primary" type="submit" onClick={e => handleSubmit(e)}>Next</Button>
+                </fieldset>}
+            <Button 
+                variant="primary" 
+                type="submit" 
+                onClick={e => handleSubmit(e)}
+            >
+            Next
+            </Button>
             </Form>
         </div>
   )
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,35 +1,82 @@
 import { useState, useEffect } from "react"
-import { Outlet } from "react-router-dom"
+import { Outlet, useParams} from "react-router-dom"
 import './Quiz.css'
 
 function Quiz() {
+    let params = useParams()
     
-    const [questions, setQuestions] = useState([])
-    const [page, setPage] = useState(0)
-    const [score, setScore] = useState(0)
+    const [state, setState] = useState({
+        questions: [],
+        question: {
+            id: null,
+            question: '',
+            alternatives: [],
+            correct_answer: ''
+        },
+        form: {
+            value: 0
+        },
+        functions: {
+            handleFormChange: e => handleFormChange(e),
+            handleSubmit: e => handleSubmit(e)
+        }
+    })
 
-    const [timer, setTimer] = useState(0)
-    const [timerId, setTimerId] = useState(null)
+    const [page, setPage] = useState(0)
+    // const [score, setScore] = useState(0)
+    
+    // const [timer, setTimer] = useState(0)
+    // const [timerId, setTimerId] = useState(null)
     
     // Obter questões pela API do Open Trivia DB (https://opentdb.com)
     function fetchQuestions() {
         fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple')
             .then(res => res.json())
             .then(data => {
-                setQuestions(data.results)
-                // console.log(data);
+                if (data.results) {
+                    setState(prevState => {
+                        return {
+                            ...prevState, 
+                            questions: data.results
+                        }
+                    })
+                }
+            })
+            .catch(e => {
+                console.error('API error: ', e);
             })
     }
+
+    // Ao obter questões, renderizar uma questão por vez
+    useEffect(() => {
+        // Selecionar a questão o array pelo índice usando parâmetro da URL
+        state.questions.forEach((element, index) => {
+            if (index == params.questionIndex) {
+                const selected_question = {
+                    id: index,
+                    question: element.question,
+                    alternatives: element.incorrect_answers.concat(element.correct_answer),
+                    correct_answer: element.correct_answer
+                }
+                setState(prevState => {
+                    return {
+                        ...prevState,
+                        question: selected_question
+                    }
+                })
+            }
+        });
+    }, [state.questions])
 
     // Quando o componente renderiza (componentDidMount)
     useEffect(() => {
         fetchQuestions()
     }, [])
-        
+    
     return (
         <div id="quizz_container">
             <h1>{`Question ${page + 1}`}</h1>
-            <Outlet context={[questions, setQuestions]} />
+            <Outlet context={[state, setState]} />
         </div>
     )
 }
